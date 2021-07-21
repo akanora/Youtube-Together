ff7542bc79d429b677d8bd33639baf8394395537
Nora <50810398+akanora@users.noreply.github.com>
2021-07-21 11:58:12 +0300

    Update youtubetogether.js

--- youtubetogether.js
+++ youtubetogether.js
@@ -217,7 +217,7 @@ console.info("[BİLGİ] Youtube Together - by Nora");
 
   config.onAfterLoad(client);
 
-  await client.login(process.env.token);
+  await client.login(config.clientToken);
   console.info("[INFO] Connected to Discord!", client.user.tag);
   config.onReady(client);
 })();
