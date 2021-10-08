a51c592b2cb10f707927cf1f6bbb2d1cdc21336b
Nora <50810398+akanora@users.noreply.github.com>
2021-10-09 00:07:46 +0300

    Delete youtubetogether.js

--- youtubetogether.js
+++
@@ -1,7 +0,0 @@
-const { ShardingManager } = require('discord.js');
-
-const manager = new ShardingManager('./index.js', { token: 'token', totalShards: 'auto', respawn: true });
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-
-manager.spawn();
